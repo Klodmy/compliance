b6b6ebab84636f6b704fd21f8396dc4f434db48e
Klodmy <kds94box@gmail.com>
2025-07-10 21:49:05 -0400

    fixed bug

--- utils.py
+++ utils.py
@@ -68,7 +68,7 @@ def get_submission_status(docs):
 
 # saving files to s3
 # getting envvars
-s3 = boto3.client("s3", aws_access_key_id = os.getenv("AWS_ACCSS_KEY_ID"),  aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY", region_name = os.getenv("AWS_REGION")))
+s3 = boto3.client("s3", aws_access_key_id = os.getenv("AWS_ACCSS_KEY_ID"),  aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY"), region_name = os.getenv("AWS_REGION"))
           
 BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
 
